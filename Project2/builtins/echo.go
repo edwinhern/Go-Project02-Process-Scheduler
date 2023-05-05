ef6380656c681fcd97d5797d68d6a74434e25c97
Morgan, Joshua <joshuamorgan2@my.unt.edu>
2023-05-05 13:23:51 -0500

    [Echo] Created Echo command and add the command to main

--- Project2/builtins/echo.go
+++ Project2/builtins/echo.go
@@ -2,12 +2,54 @@ package builtins
 
 import (
 	"fmt"
-	"io"
 )
 
-func Echo(w io.Writer, args ...string) error {
-	s := args[0]
-	_, err := fmt.Fprintf(w, "%s\n", s)
+func Echo(args ...string) error {
+	switch len(args) {
+	case 0:
+		// No argument given
+		fmt.Printf("%s\n", "")
+	default:
+		for i := 0; i < len(args); i++ {
+			str := args[i]
+			// Print arg if " not found at beginning of argument
+			if str[0] != '"' {
+				fmt.Printf("%s\n", str)
+			} else {
+				var start = i
+				var count = i
+				var endChar = str[len(str)-1]
 
-	return err
+				// Loop until finding arg that ends in "
+				for endChar != '"' {
+					i++
+					count++
+					str = args[i]
+					endChar = str[len(str)-1]
+				}
+
+				// Print of the arguments in "", The arguments with a " have them deleted before output to terminal
+				for j := start; j <= count; j++ {
+					if j == start && j == count {
+						catStr := args[j]
+						catStr = catStr[1 : len(catStr)-1]
+						fmt.Printf("%s ", catStr)
+					} else if j == start && j != count {
+						catStr := args[j]
+						catStr = catStr[1:]
+						fmt.Printf("%s ", catStr)
+					} else if j != start && j == count {
+						catStr := args[j]
+						catStr = catStr[:len(catStr)-1]
+						fmt.Printf("%s ", catStr)
+					} else {
+						catStr := args[j]
+						fmt.Printf("%s ", catStr)
+					}
+				}
+				fmt.Printf("%s\n", "")
+			}
+		}
+	}
+	return nil
 }
